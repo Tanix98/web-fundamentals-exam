64f026dafaa0c1d35e912f588fdf4cbd6f62592a
Tanix98 <oysteinrostvik@protonmail.com>
2023-11-28 13:42:51 +0100

    move extra files to other directory

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 set -x
 
+# set variables
+# change user_name to your own raspberry pi username
+user_name="oystrost"
+page_url="https://en.wikipedia.org/wiki/List_of_municipalities_of_Norway"
+other_folder="/home/${user_name}/Desktop/exam-stuff/grade-d/other"
+page_file="${other_folder}/original.page.html.txt"
+page_file_one_line="${other_folder}/page.html.one.line.txt"
+page_file_table_newline="${other_folder}/page.html.table.newline.txt"
+table_only="${other_folder}/table.only.txt"
+final_page="page.html"
+
+# Create folder
+mkdir -p "$other_folder"
+
 # Download the webpage to the local machine
-curl -s "https://en.wikipedia.org/wiki/List_of_municipalities_of_Norway" > "page.html.txt"
+curl -s "$page_url" > "$page_file"
 
 # Remove whitespace characters and tabs
-cat "html.txt" | tr -d '\n\t' > "page.html.one.line.txt"
-sed 's|<table class="sortable wikitable">|\n<table class="sortable wikitable">|'"page.html.one.line.txt" | 
-    sed 's|</table>|</table>\n|g' > "page.html.table.newline.txt"
+cat "html.txt" | tr -d '\n\t' > "$page_file_one_line"
+sed 's|<table class="sortable wikitable">|\n<table class="sortable wikitable">|'"$page_file_one_line" | 
+    sed 's|</table>|</table>\n|g' > "$page_file_table_newline"
 
 # only take the third line
-sed -n '3p' "page.html.table.newline.txt" > "table.only.txt"
+sed -n '3p' "$page_file_table_newline" > "$table_only"
 
 # string literal - our future page -- inserting extracted table as body content
 page_template='
@@ -22,12 +36,12 @@ page_template='
     <title>sfgsfsfdsfd</title>
 </head>
 <body>
-    '"$(cat "table.only.txt")"'
+    '"$(cat "$table_only")"'
 </body>
 </html>
 '
 
 # dumping variable contents into a file
-echo "$page_template" > "our.page.html"
+echo "$page_template" > "$final_page"
 
 $SHELL
